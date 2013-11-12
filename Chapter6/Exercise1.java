ea0d68878b7c11d5dcf62acea768329b800e88b9
Jarrett Beck <jbeck@java.nas.local>
2013-11-12 10:08:01 -0500

    first commit

---
+++ Chapter6/Exercise1.java
@@ -0,0 +1,23 @@
+
+class Exercise1 {
+
+  public static void main(String[] args){
+    System.out.println(" a==4, b==8");
+  {
+   int a;
+   int b;
+    a = 4;
+    b = 8;
+        boolean flag;
+         flag = true;
+        boolean testResult = false;
+        boolean evenFlag = (a/b == 0);
+        boolean positiveFlag = (a > 0);
+    if (evenFlag) {
+      System.out.println("true");
+    }else{
+      System.out.println("false");
+  }
+ }
+}
+}
---
+++ Chapter6/Factorial.java
@@ -0,0 +1,17 @@
+class Factorial {
+  public static void main(String[ ] args) {
+    System.out.println(factorial(3));
+  }
+
+  public static int factorial(int n) {
+    if(n == 0) {
+      return 1;
+    } else {
+       int recurse = factorial (n-1);
+       int result = n * recurse;
+       return result;
+       }
+    }
+}
+
+
