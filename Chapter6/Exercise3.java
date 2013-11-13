a1e7cc90cc4ae36c4c88c15a2a32de76c2f1f9e6
Jarrett Beck <jbeck@java.nas.local>
2013-11-13 10:16:40 -0500

    Added Exercise 6.3

---
+++ Chapter6/Exercise3.java
@@ -0,0 +1,16 @@
+class Exercise3 {
+  public static void main(String[] args) {
+	isTriangle(a,b,c);
+	 int a =1;
+	 int b =2;
+	 int c =3;
+  }
+  public static boolean isTriangle(int a, int b, int c){
+	boolean flag;
+	flag = true;
+	  return a,b,c;
+	boolean testResult = false;
+  }
+ }
+
+
