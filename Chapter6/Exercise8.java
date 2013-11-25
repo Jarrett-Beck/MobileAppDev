f98663e0bbfa672e270ca9d455739c33d181d6b9
Jarrett Beck <jbeck@java.nas.local>
2013-11-25 10:16:32 -0500

    Exercise8

---
+++ Chapter6/Exercise8.java
@@ -0,0 +1,14 @@
+
+class Exercise8 {
+    public static void main(String[] args){
+  // last: returns a new String that contains all but the
+  // first letter of the given String
+    public static String rest(String s) {
+	return s.substring(1, s.length());
+ }
+}
+  // length: returns the length of the given String
+    public static int length(String s) {
+	return s.length();
+ }
+}
